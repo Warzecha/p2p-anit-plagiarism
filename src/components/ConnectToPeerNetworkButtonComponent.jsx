6496dc7876493a39a19de9adfce1e990ccb8abe4
Torzewicz, Michal <torzewicz.michal@gmail.com>
2020-01-23 14:43:20 +0100

    develop

--- src/components/ConnectToPeerNetworkButtonComponent.jsx
+++ src/components/ConnectToPeerNetworkButtonComponent.jsx
@@ -1,12 +1,16 @@
 import React, {useState} from "react";
-import {makeStyles} from "@material-ui/core";
+import {makeStyles, TextField} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import CircularProgress from '@material-ui/core/CircularProgress';
+
 const {ipcRenderer} = require('electron');
 
 const ConnectToPeerNetworkButton = (props) => {
     const styles = useStyles();
-
+    const [selfAddress, setSelfAddress] = useState(null);
+    const [validAddress, setValidAddress] = useState(true);
+    const [broadcastAddress, setBroadcastAddress] = useState(null);
+    const [validBroadcast, setValidBroadcast] = useState(true);
     const [isConnected, setIsConnected] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -19,12 +23,50 @@ const ConnectToPeerNetworkButton = (props) => {
 
     const handleClick = () => {
         setIsLoading(true);
-        ipcRenderer.send('connectToPearNetwork')
+        ipcRenderer.send('connectToPearNetwork', {
+            selfAddress: selfAddress,
+            broadcastAddress: broadcastAddress
+        })
+    };
+
+    const checkValidIpAddress = (stringAddress) => {
+        let numberOfDots = stringAddress.match(/\./g);
+        if (numberOfDots == null || numberOfDots.length < 3) {
+            setValidAddress(false)
+        } else if (stringAddress.length < 7) {
+            setValidAddress(false)
+        } else {
+            setValidAddress(true)
+        }
+    };
+
+    const checkValidIpBroadBastAddress = (stringAddress) => {
+        let numberOfDots = stringAddress.match(/\./g);
+        if (numberOfDots == null || numberOfDots.length < 3) {
+            setValidBroadcast(false)
+        } else if (stringAddress.length < 7) {
+            setValidBroadcast(false)
+        } else {
+            setValidBroadcast(true)
+        }
     };
 
     return (
         <div className={styles.root}>
-            <Button disabled={isConnected} variant={"contained"} onClick={handleClick}>Connect</Button>
+            <TextField label={"Machine address"} error={!validAddress}
+                       onChange={(event) => {
+                           checkValidIpAddress(event.target.value);
+                           setSelfAddress(event.target.value)
+                       }}/>
+            <TextField label={"Broadcast address"} error={!validBroadcast}
+                       onChange={(event) => {
+                           checkValidIpBroadBastAddress(event.target.value);
+                           setBroadcastAddress(event.target.value);
+                       }}/>
+            <Button style={{
+                marginTop: '10px'
+            }} disabled={isConnected && !validAddress && !validBroadcast} variant={"contained"}
+                    onClick={handleClick}>Connect</Button>
             {isLoading && <CircularProgress className={styles.progress}/>}
         </div>
     )
